c8a03f3332d7bfeaa3464092e22936865f0e4f81
pellegrineschi <pellegrineschi86@gmail.com>
2025-03-01 21:31:57 -0300

    render

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,27 @@ const socket = io()
 
 socket.on('all-message', (data)=>{
  console.log(data);
+ render(data)
+ let chat = document.getElementById('caja')
+ chat.scrollTop = chat.scrollHeight
  
 })
+
+const render = (data)=>{
+
+   const html = data.map(elem=>{
+        return(
+        `
+        <div>
+            <strong>${elem.dominio}</strong> oblea <em>${elem.oblea}</em>
+        </div>
+        `  
+        )
+        
+    }).join(' ')
+    document.getElementById('caja').innerHTML = html
+
+}
 
 const addMensage = ()=>{
     const msg = {
